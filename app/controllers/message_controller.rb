c5f0e76798983bc66a283483d6e67035d2712d9a
Chris Rueber <crueber@gmail.com>
2011-06-06 21:52:45 -0500

    Initial commit

---
+++ app/controllers/message_controller.rb
@@ -0,0 +1,24 @@
+class MessageController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+  end
+
+  def create
+    @m = Message.new
+    @m.content = params[:message][:content]
+    @m.author = current_user.username
+
+    if @m.save
+      redirect_to :controller => 'home', :action => 'index'
+    end
+  end
+
+  def update
+
+  end
+
+  def delete
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ignis::Application.config.secret_token = 'f1fd23f1d3178a4d8a7e30d9ea12a0ee92e308f483c136c4a28ffd05f457bb2d37c713fa0f4dde5b27021fe3b1d6e70b556c22fa65a16a3022b04b4cafe983d5'
