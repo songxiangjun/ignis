1d194209deebde76f02ee7767d8cc553b627b71a
Chris Rueber <crueber@bluesiren.net>
2011-06-21 19:01:59 -0500

    Gearing up for restricting access to only groups that a user belongs to!

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
+class Membership < ActiveRecord::Base
+  belongs_to :group
+  belongs_to :user
+end
+
 # Users are central to the application. They have messages and groups (and memberships, which tells about their admin level on a group)
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
@@ -10,7 +15,9 @@ class User < ActiveRecord::Base
 
   has_many :messages
   has_many :memberships
-  has_many :groups, :through => :memberships
+  has_many :admin_memberships,   :class_name => "Membership",    :conditions => { :is_admin => true }
+  has_many :admin_access_groups, :through => :admin_memberships, :source => :group
+  has_many :accessible_groups,   :through => :memberships,       :source => :group
 
   # Used on a base model of User in order to find the last five minutes of users that have been seen.
   def self.recent
